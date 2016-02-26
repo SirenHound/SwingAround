f70fe03b5cdd992a69736f72c74cdd49dd292888
Ethan McCoy <SirenHound@users.noreply.github.com>
2016-02-26 09:07:44 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 (function(w, d){
   d.onreadystatechange = function(){
-    if (d.readystate === "interactive"){
+    if (d.readyState === "interactive"){
       var mapObj = new L.Map('map');
       console.log("Here's the map", mapObj);
     }
