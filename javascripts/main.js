0453f58975d42561d1ddad2d1de6b760b73cb44e
Ethan McCoy <SirenHound@users.noreply.github.com>
2016-02-26 09:01:25 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,8 @@
-var mapObj = new L.Map('map');
-console.log("Here's the map", mapObj);
+(function(w, d){
+  d.onreadystatechange = function(){
+    if (d.readystate = "interactive"){
+      var mapObj = new L.Map('map');
+      console.log("Here's the map", mapObj);
+    }
+  }
+})(window, document);
