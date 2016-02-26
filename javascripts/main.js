8659dcb7c5be9fdbc103ebd2def6d77d3ceef8ca
Ethan McCoy <SirenHound@users.noreply.github.com>
2016-02-26 09:06:50 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,5 +4,8 @@
       var mapObj = new L.Map('map');
       console.log("Here's the map", mapObj);
     }
+    else{
+      console.info("readystate", d.readystate);
+    }
   }
 })(window, document);
