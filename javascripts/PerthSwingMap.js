17c4339035b07594f3b74e59e2582cfe7048eced
Ethan McCoy <SirenHound@users.noreply.github.com>
2016-07-09 15:04:05 +0800

    Update PerthSwingMap.js

--- javascripts/PerthSwingMap.js
+++ javascripts/PerthSwingMap.js
@@ -1,17 +1,29 @@
-var map = L.map('danceMap').setView({lat: -31.93409949029348, lng: 115.85100173950197}, 13);
+(function(w, d){
+  d.onreadystatechange = function(){
+    if (d.readyState === "interactive"){
+      var map = L.map('danceMap').setView({lat: -31.93409949029348, lng: 115.85100173950197}, 13);
+      
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+      
+      if ("geolocation" in navigator) {
+        /* geolocation is available */
+        navigator.geolocation.getCurrentPosition(function(position) {
+          //alert("L");
+          var latLng = L.latLng(position.coords.latitude, position.coords.longitude);
+          //alert("latlng: "+ latLng.toString());
+          L.circleMarker(latLng, {}).addTo(map);
+          map.panTo(latLng);
+        });
+      } else {
+        /* geolocation IS NOT available */
+        alert("No Location");
+      }
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
-if ("geolocation" in navigator) {
-  /* geolocation is available */
-  navigator.geolocation.getCurrentPosition(function(position) {
-    //alert("L");
-    var latLng = L.latLng(position.coords.latitude, position.coords.longitude);
-    //alert("latlng: "+ latLng.toString());
-    L.circleMarker(latLng, {}).addTo(map);
-    map.panTo(latLng);
-  });
-} else {
-  /* geolocation IS NOT available */
-  alert("No Location");
-}
+      console.log("Here's the map", mapObj);
+    }
+    else{
+      console.info("readystate", d.readystate);
+    }
+  }
+})(window, document);
