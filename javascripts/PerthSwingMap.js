92457605acfcd9c0358b8355cfccb49a0a3e73d6
Ethan McCoy <SirenHound@users.noreply.github.com>
2016-07-09 15:12:06 +0800

    Update PerthSwingMap.js

--- javascripts/PerthSwingMap.js
+++ javascripts/PerthSwingMap.js
@@ -3,7 +3,7 @@
     if (d.readyState === "interactive"){
       var mapObj = L.map('danceMap').setView({lat: -31.93409949029348, lng: 115.85100173950197}, 13);
       
-      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mapObj);
       
       if ("geolocation" in navigator) {
         /* geolocation is available */
@@ -11,8 +11,8 @@
           //alert("L");
           var latLng = L.latLng(position.coords.latitude, position.coords.longitude);
           //alert("latlng: "+ latLng.toString());
-          L.circleMarker(latLng, {}).addTo(map);
-          map.panTo(latLng);
+          L.circleMarker(latLng, {}).addTo(mapObj);
+          mapObj.panTo(latLng);
         });
       } else {
         /* geolocation IS NOT available */
